abd2380f1a7dae357acae1d8693a48207be7caf1
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2026-03-27 13:11:59 +0300

    first commit

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'https://panel.goadisyon.com/';
+axios.defaults.baseURL = import.meta.env.VITE_URL;
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem("token");
